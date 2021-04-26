a1eb3f8abdee805125720e26e53eb7ec980dab3b
websterguy <54455090+websterguy@users.noreply.github.com>
2021-04-25 20:57:55 -0700

    fix await/async problem

--- deleteSummonsActors.js
+++ deleteSummonsActors.js
@@ -2,8 +2,17 @@ const config = {
     folderName: "Summons" // match actor folder where summons are created
 };
 
+
 let folder = game.folders.find(o => o.type === "Actor" && o.name === config.folderName);
 
-while (folder.content.length > 0) {
-    await folder.content[0].delete();
+if (!game.user.isGM) ui.notifications.warn("User is not GM. Please run macro as GM.");
+else if (!folder) ui.notifications.warn('No actors folder named "' + config.folderName + '". Please check config.');
+else {
+    deleteFolderContents(folder);
+}
+
+async function deleteFolderContents(folder) {
+    while (folder.content.length > 0) {
+        await folder.content[0].delete();
+    }
 }
